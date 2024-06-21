dbc502705f9bd93038be2397c9c274d5f33f0f39
xiaodong.dai <xiaodong.dai@autoliv.com>
2024-06-21 10:06:36 +0800

    support multiple anchor points

--- wikipedia-crawler.py
+++ wikipedia-crawler.py
@@ -9,7 +9,11 @@ from urllib.parse import urlparse
 
 import requests
 from bs4 import BeautifulSoup
+from torch import cosine_similarity
 import urllib3
+import json
+import httpx
+from openai import OpenAI
 
 # Suppress only the single InsecureRequestWarning from urllib3 needed
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -19,44 +23,65 @@ DEFAULT_INTERVAL = 5.0  # interval between requests (seconds)
 DEFAULT_ARTICLES_LIMIT = 1  # total number articles to be extrated
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
 
-visited_urls = set()  # all urls already visited, to not visit twice
+
 pending_urls = []  # queue
 
 
 def load_urls(session_file):
     """Resume previous session if any, load visited URLs"""
-
+    visited_urls = set()  # all urls already visited, to not visit twice
     try:
         with open(session_file) as fin:
             for line in fin:
                 visited_urls.add(line.strip())
     except FileNotFoundError:
         pass
+    
+    return visited_urls
+    
+import re
+
+MANUFACTURING_KEYWORDS = [
+    'manufacturing', 'industrial', 'production', 'factory', 'machinery',
+    'automation', 'assembly', 'fabrication', 'process', 'engineering'
+]
+
+# Calculate cosine similarity
+from scipy.spatial.distance import cosine
+def cosine_similarity(vec1, vec2):
+    return 1 - cosine(vec1, vec2)
+
+def is_relevant(anchor_embeddings, text, embedding_model, openai_client, required_similarity):
+    embedding = get_embedding_by_text(text, embedding_model, openai_client)
+    max_similarity = 0
+    for anchor_embedding in anchor_embeddings:
+        #convert the embeddings to pytorch tensors
+        
+        similarity = cosine_similarity(embedding, anchor_embedding)
+        if similarity > max_similarity:
+            max_similarity = similarity
+        if similarity > required_similarity:
+            return True, max_similarity
+    return False, max_similarity
 
 
-def scrap(base_url, article, output_file, session_file):
+def scrap(base_url, article, max_tokens_per_article=8192):
     """Represents one request per article"""
 
     full_url = base_url + article
-    try:
-        r = requests.get(full_url, headers={'User-Agent': USER_AGENT}, verify=False)
-    except requests.exceptions.ConnectionError:
-        print("Check your Internet connection")
-        input("Press [ENTER] to continue to the next request.")
-        return
+    r = requests.get(full_url, headers={'User-Agent': USER_AGENT}, verify=False)
+    
     if r.status_code not in (200, 404):
         print("Failed to request page (code {})".format(r.status_code))
-        input("Press [ENTER] to continue to the next request.")
-        return
+        time.sleep(10)
+        raise Exception("Failed to request page (code {})".format(r.status_code))
 
     soup = BeautifulSoup(r.text, 'html.parser')
     content = soup.find('div', {'id':'mw-content-text'})
 
-    with open(session_file, 'a') as fout:
-        fout.write(full_url + '\n')  # log URL to session file
-
     # add new related articles to queue
     # check if are actual articles URL
+    hrefs = []
     for a in content.find_all('a'):
         href = a.get('href')
         if not href:
@@ -67,22 +92,14 @@ def scrap(base_url, article, output_file, session_file):
             continue
         elif href[-4:] in ".png .jpg .jpeg .svg":  # ignore image files inside articles
             continue
-        elif base_url + href in visited_urls:  # already visited
-            continue
-        if href in pending_urls:  # already added to queue
-            continue
-        pending_urls.append(href)
-
-    # skip if already added text from this article, as continuing session
-    if full_url in visited_urls:
-        return
-    visited_urls.add(full_url)
+        hrefs.append(href)
 
     parenthesis_regex = re.compile('\(.+?\)')  # to remove parenthesis content
     citations_regex = re.compile('\[.+?\]')  # to remove citations, e.g. [1]
 
     # get plain text from each <p>
     p_list = content.find_all('p')
+    all_text = ''
     with open(output_file, 'a', encoding='utf-8') as fout:
         for p in p_list:
             text = p.get_text().strip()
@@ -95,36 +112,142 @@ def scrap(base_url, article, output_file, session_file):
                 continue  # Skip paragraphs with short lines
             
             if text.endswith('.'):
-                fout.write(text + '\n\n')  # extra line between paragraphs
+                if num_tokens(all_text + text) > max_tokens_per_article:
+                    break
+                all_text += text + '\n'
+                
+    return all_text, hrefs
+
+import tiktoken
+
+def num_tokens(text):
+    encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    return len(encoding.encode(text))
 
 
-def main(initial_url, articles_limit, interval, output_file):
+def get_embedding_by_url(url, embedding_model, openai_client):
+    # fetch the text from the given url
+    base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(url))
+    article = url[len(base_url):]
+    all_text, _ = scrap(base_url, article)
+    response = openai_client.embeddings.create(
+        input=all_text,
+        model=embedding_model
+    )
+    return response.data[0].embedding
+
+def get_embedding_by_text(text, embedding_model, openai_client):
+    # retry 3 times
+    retry = 0
+    max_retry = 3
+    while retry < max_retry:
+        try:
+            response = openai_client.embeddings.create(
+                input=text,
+                model=embedding_model
+            )
+            return response.data[0].embedding
+        except Exception as e:
+            print(f"Error: {e}, retrying...")
+            time.sleep(1)
+            retry += 1
+            continue
+
+
+def get_anchor_embeddings(anchors, embedding_model, openai_client):
+    anchor_embeddings = []
+    for anchor in anchors:
+        print(f"Getting embeddings for anchor: {anchor}")
+        # Get embeddings for anchor
+        anchor_embedding = get_embedding_by_url(anchor, embedding_model, openai_client)
+        anchor_embeddings.append(anchor_embedding)
+    return anchor_embeddings
+
+
+def main(anchors, embedding_model, openai_client, articles, interval, output_file, max_tokens_per_article, required_similarity):
     """ Main loop, single thread """
 
-    minutes_estimate = interval * articles_limit / 60
-    print("This session will take {:.1f} minute(s) to download {} article(s):".format(minutes_estimate, articles_limit))
+    minutes_estimate = interval * articles / 60
+    print("This session will take {:.1f} minute(s) to download {} article(s):".format(minutes_estimate, articles))
     print("\t(Press CTRL+C to pause)\n")
     session_file = "session_" + output_file
-    load_urls(session_file)  # load previous session (if any)
-    base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(initial_url))
-    initial_url = initial_url[len(base_url):]
-    pending_urls.append(initial_url)
-
+    pending_file = "pending_" + output_file
+    visited_urls = load_urls(session_file)  # load previous session (if any)
+    pending_urls = list(load_urls(pending_file))
+    
+    anchor_embeddings = get_anchor_embeddings(anchors, embedding_model, openai_client)
+    
+    
+    base_url = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(anchors[0]))
+    
+    # first time:
+    if len(pending_urls) == 0 and len(visited_urls) == 0:
+        for anchor in anchors:
+            article = anchor[len(base_url):]
+            pending_urls.append(article)
+            
+            
+    print('pending_urls', pending_urls)
     counter = 0
     while len(pending_urls) > 0:
         try:
             counter += 1
-            if counter > articles_limit:
+            if counter > articles:
                 break
             try:
                 next_url = pending_urls.pop(0)
             except IndexError:
                 break
-
-            time.sleep(interval)
+            
+            
             article_format = next_url.replace('/wiki/', '')[:35]
             print("{:<7} {}".format(counter, article_format))
-            scrap(base_url, next_url, output_file, session_file)
+            retry = 0
+            while retry < 3:
+                try:
+                    text, hrefs = scrap(base_url, next_url, max_tokens_per_article)
+                    if text is not None:
+                        break
+                except:
+                    print(f"Failed to scrap article {next_url}, retrying...")
+                    time.sleep(10)
+                    retry += 1
+                    continue
+                    
+            if text is None:
+                print(f"Skipping article {full_url} as it has no text.")
+                continue
+            
+            full_url = base_url + next_url
+            if full_url not in visited_urls:
+                relevant, max_similarity = is_relevant(anchor_embeddings, text, embedding_model, openai_client, required_similarity)
+                if not relevant:
+                    print(f"Skipping article {full_url} as {max_similarity} is less than the required similarity: {required_similarity}.")
+                    continue
+            
+                # skip if already added text from this article, as continuing session
+                with open(output_file, 'a', encoding='utf-8') as fout:
+                    fout.write(text)
+                    fout.write('\n\n')
+                    
+                with open(session_file, 'a') as fout:
+                    fout.write(full_url + '\n')
+                
+                visited_urls.add(full_url)
+
+            
+            for href in hrefs:
+                if href not in pending_urls and href not in visited_urls and len(pending_urls) < 1000:
+                    pending_urls.append(href)
+                    
+            # overwrite pending files every 10 loops
+            if counter % 10 == 0:
+                with open(pending_file, 'w') as fout:
+                    for url in pending_urls:
+                        fout.write(url + '\n')
+                        
+            time.sleep(interval)
+            
         except KeyboardInterrupt:
             input("\n> PAUSED. Press [ENTER] to continue...\n")
             counter -= 1
@@ -134,10 +257,19 @@ def main(initial_url, articles_limit, interval, output_file):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("initial_url", help="Initial Wikipedia article, e.g. https://en.wikipedia.org/wiki/Biology")
-    parser.add_argument("-a", "--articles", nargs='?', default=DEFAULT_ARTICLES_LIMIT, type=int, help="Total number of articles")
-    parser.add_argument("-i", "--interval", nargs='?', default=DEFAULT_INTERVAL, type=float, help="Interval between requests")
-    parser.add_argument("-o", "--output", nargs='?', default=DEFAULT_OUTPUT, help="File output")
-    args = parser.parse_args()
-    main(args.initial_url, args.articles, args.interval, args.output)
+    # read file 'config.json'
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        anchors = data['anchors']
+        embedding_model = data['embedding-model']
+        api_key = data['api-key']
+        articles = data['articles']
+        interval = data['interval']
+        output_file = data['output-file']
+        max_tokens_per_article = data['max-tokens-per-article']
+        required_similarity = data['required-similarity']
+    http_client = httpx.Client(verify=False)
+    openai_client = OpenAI(api_key=api_key,  http_client=http_client)
+    main(anchors, embedding_model, openai_client, articles, interval, output_file, max_tokens_per_article, required_similarity)
+
+
